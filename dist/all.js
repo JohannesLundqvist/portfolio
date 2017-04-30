23656ff54134c20007b0b9799effeb9c0a68d469
Palina Kolas <p.kolos@hotmail.com>
2017-04-30 14:58:51 +0200

    Added content

--- dist/all.js
+++ dist/all.js
@@ -10306,81 +10306,24 @@ $(window).scroll(function() {
   });
 });
 
-//Modal stuff - comment out if acts to weird
-$(document).ready(function(){
-   $(window.location.hash).modal('show');
-   $('div[data-toggle="modal"]').click(function(){
-     console.log("opened");
-      window.location.hash = $(this).attr('href');
-      window.onhashchange = function() {
-        if (!location.hash){
-          $('.modal').modal('hide');
-        }
-      }
-   });
-   function revertToOriginalURL() {
-       var original = window.location.href.substr(0, window.location.href.indexOf('#'))
-       history.replaceState({}, document.title, original);
-   }
-
-   $('.modal').on('hidden.bs.modal', function () {
-       revertToOriginalURL();
-   });
-   $('button[data-dismiss="modal"]').click(function(){
-        var original = window.location.href.substr(0, window.location.href.indexOf('#'))
-        history.replaceState({}, document.title, original);
-        $('.modal').modal({show:false});
-    });
-       if(window.location.href.indexOf('#automanager') != -1) {
-         $('.modal').load('/automanager.html',function(result){
-            $('#automanagerModal').modal({show:true});
-               });
-       }
-       if(window.location.href.indexOf('#campaignGen') != -1) {
-         $('.modal').load('/campaignGen.html',function(result){
-            $('#campaignGenModal').modal({show:true});
-               });
-       }
-       if(window.location.href.indexOf('#honeycomb') != -1) {
-         $('.modal').load('/honeycomb.html',function(result){
-            $('#honeycombModal').modal({show:true});
-            $('#honeycombModal').on( 'show.bs.modal', function( event ) {
-              $('.main-carousel').flickity();
-            });
-               });
-       }
-       if(window.location.href.indexOf('#turnApp') != -1) {
-         $('.modal').load('/turnApp.html',function(result){
-            $('#turnAppModal').modal({show:true});
-               });
-       }
-       if(window.location.href.indexOf('#bellaStel') != -1) {
-         $('.modal').load('/bellaStel.html',function(result){
-            $('#bellaStelModal').modal({show:true});
-               });
-       }
-       if(window.location.href.indexOf('#ixdRedesignGen') != -1) {
-         $('.modal').load('/ixdRedesignGen.html',function(result){
-            $('#ixdRedesignGenModal').modal({show:true});
-               });
-       }
-       if(window.location.href.indexOf('#hamlin') != -1) {
-         $('.modal').load('/hamlin.html',function(result){
-            $('#hamlinModal').modal({show:true});
-               });
-       }
-
+//Project carousels
+$('.carpool-carousel').flickity({
+  wrapAround: true
 });
-
-// show carousel after modal shown
-$('#honeycombModal').on( 'show.bs.modal', function( event ) {
-  $('.main-carousel').flickity();
+$('.honeycomb-carousel').flickity({
+  wrapAround: true
+});
+$('.turnapp-carousel').flickity({
+  wrapAround: true
+});
+$('.bellastel-carousel').flickity({
+  wrapAround: true
 });
 
 //var screenWidth = $(window).height()-300;
 //$("#hero").height(screenWidth);
 
-//Opens modals
+//Mobile menu
 $('.collapse').on('show.bs.collapse', function(e) {
   $('#navbar').addClass("navbar-fullscreen");
     });
@@ -10392,39 +10335,3 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
                 $('#navmenu').collapse('hide');
             });
         }
-$('#automanager').click(function(){
-    $('.modal').load('/automanager.html',function(result){
-       $('#automanagerModal').modal({show:true});
-        	});
-});
-$('#campaignGen').click(function(){
-    $('.modal').load('/campaignGen.html',function(result){
-       $('#campaignGenModal').modal({show:true});
-        	});
-});
-$('#honeycomb').click(function(){
-    $('.modal').load('/honeycomb.html',function(result){
-       $('#honeycombModal').modal({show:true});
-       console.log('click');
-        	});
-});
-$('#turnApp').click(function(){
-    $('.modal').load('/turnApp.html',function(result){
-       $('#turnAppModal').modal({show:true});
-        	});
-});
-$('#bellaStel').click(function(){
-    $('.modal').load('/bellaStel.html',function(result){
-       $('#bellaStelModal').modal({show:true});
-        	});
-});
-$('#ixdRedesign').click(function(){
-    $('.modal').load('/ixdRedesign.html',function(result){
-       $('#ixdRedesignModal').modal({show:true});
-        	});
-});
-$('#hamlin').click(function(){
-    $('.modal').load('/hamlin.html',function(result){
-       $('#hamlinModal').modal({show:true});
-        	});
-});
